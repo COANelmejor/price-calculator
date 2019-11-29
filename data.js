46687f9e3f9694a1182c8f01e901708997d3aa3c
Christian Arana <COANelmejor@gmail.com>
2019-11-29 00:49:27 -0600

    Sistema terminado en versión 1 Falta agregar documentación y data al readme.md

--- data.js
+++ data.js
@@ -1,7 +1,7 @@
 'use strict';
 
 /**
- * # Precio de hora Normal
+ * ## Precio de hora Normal
  * 
  * Este es el precio por hora que cobras normalmente.
  * En un ambiente normal este debe ser tu precio por hora
@@ -9,7 +9,7 @@
 const precioHoraNormal = 30;
 
 /**
- * # Precio Hora Extra
+ * ## Precio Hora Extra
  * 
  * Este es tu precio por hora extra.
  * 
@@ -18,67 +18,102 @@ const precioHoraNormal = 30;
 const precioHoraExtra = 40;
 
 /**
- * # Máximo de horas al día
+ * ## Máximo de horas al día
  * 
- * Este es el máximo de horas que puedes trabajar al día.
+ * [Number] con los máximos de horas que puedes trabajar al día.
+ * De forma predeterminada son 10 horas por cada día
  */
-const maxHorasDia = 10;
+// const maxHorasDia = {
+//     Mon: 10,
+//     Tue: 10,
+//     Wed: 10,
+//     Thu: 10,
+//     Fri: 10,
+//     Sat: 10,
+//     Sun: 10,
+// };
 
 /**
- * # Inicio de Hora Extra
+ * ## Inicio de Hora Extra
  * 
  * A partir de esta hora y las horas siguientes se calcularán con el precio de hora Extra.
  */
 const inicioHoraExtra = 5
 
 /**
- * # Multiplicador de precio para Fines de semana
+ * ## Multiplicador de Fines de semana
  * 
- * Este multiplicador cambiará el precio de las horas para los fines de semana o días festivos.
+ * Este multiplicador cambiará el precio de las horas para los fines de semana.
  * 
  * Ejemplo: Si tu precio por hora normal son 30 y el multiplicador es 2, tu precio por hora normal 
  * en fin de semana será de 60.
  */
-const horasFindeMiltiplier = 2;
+const precioFindeMiltiplier = 2;
 
 /**
- * # Fin de semana
+ * ## Fin de semana
  * 
- * String[] que tiene los días considerados como parte del fin de semana
+ * [String] que tiene los días considerados como parte del fin de semana.
+ * 
+ * Los valores deben ir en minúsculas en su version abreviada a 3 letra en inglés: mon, tue, etc.
  */
 const diasFinde = [
-    'Sat', 'Sun'
+    'sat', 'sun'
 ]
 
-const diasFesitvos = [
-    '15-dic',
-    '16-dic',
-    '17-dic',
-    '18-dic',
-    '19-dic',
-    '20-dic',
-    '21-dic',
-    '22-dic',
-    '23-dic',
-    '24-dic',
-    '25-dic',
-    '26-dic',
-    '27-dic',
-    '28-dic',
-    '29-dic',
-    '30-dic',
-    '31-dic',
-    '01-ene',
-    '02-ene',
-    '03-ene',
-    '04-ene',
-    '05-ene',
+/**
+ * ## Multiplicador de días Festivos
+ * 
+ * Este multiplicador cambiará el precio de las horas para los días festivos.
+ * 
+ * De forma predeterminada copia el valor de precioFindeMiltiplier.
+ * 
+ * Ejemplo: Si tu precio por hora normal son 30 y el multiplicador es 2, tu precio por hora normal 
+ * en fin de semana será de 60.
+ */
+const precioFestivosMiltiplier = precioFindeMiltiplier;
+
+/**
+ * ## Días Festivos
+ * 
+ * [String] que tiene Días considerados Fesitvos.
+ * 
+ * El formato debe ser **"dd-mmm"** donde **"dd"** es el día en 2 dígitos: 00, 01, 02, etc. y 
+ * **"mmm"** es el mes en formato de 3 letras en inglés: "jan", "feb", "mar", etc.
+ */
+const diasFestivos = [
+    '16-dec',
+    '17-dec',
+    '18-dec',
+    '19-dec',
+    '20-dec',
+    '21-dec',
+    '22-dec',
+    '23-dec',
+    '24-dec',
+    '25-dec',
+    '26-dec',
+    '27-dec',
+    '28-dec',
+    '29-dec',
+    '30-dec',
+    '31-dec',
+    '01-jan',
+    '02-jan',
+    '03-jan',
+    '04-jan',
+    '05-jan',
 ]
+
+const locale = 'es';
 
 module.exports = {
-    precioHoraExtra,
+    inicioHoraExtra,
     precioHoraNormal,
-    horasFindeMiltiplier,
+    precioHoraExtra,
     diasFinde,
-    diasFesitvos
+    precioFindeMiltiplier,
+    diasFestivos,
+    precioFestivosMiltiplier,
+    locale
 }
--- handler.js
+++ handler.js
@@ -1,18 +1,122 @@
 'use strict';
 
-const moment = require('moment');
-const datos = require('./data');
-
 module.exports.hello = async event => {
-  const ahora = moment().locale('es').utcOffset(-6).format('LLL');
+  const moment = require('moment');
+  const d = require('./data');
+  const q = event.queryStringParameters || {};
+
+
+  const fecha = q.fecha || moment().hour(0).minute(0).add(1, 'days').format('DD-MMM').toLowerCase();
+  let year = q.year || moment().hour(0).minute(0).add(1, 'days').format('YYYY').toLowerCase();
+  let fechasplit = fecha.split('-');
+
+  console.log(fecha);
+  console.log(fechasplit);
+
+  const horasxdia = parseInt(q.horasxdia) || 8;
+  const totalhoras = parseInt(q.totalhoras) || 8;
+  let finde = q.finde || false;
+  finde = (finde == 'true') ? true : false;
+
+  let dias = [];
+  let trabajoxdia = [];
+  let totaldias = 0;
+
+  for (let i = totalhoras; i > 0; i -= horasxdia) {
+    if (horasxdia <= i) {
+      dias.push(horasxdia);
+    } else {
+      dias.push(i);
+    }
+  }
+
+  for (let i = 0; i < dias.length; i++) {
+    const horas = dias[i];
+    let trabajo = {}
+    const diainicio = moment()
+      .hour(0).minute(0)
+      .date(fechasplit[0])
+      .month(fechasplit[1])
+      .year(parseInt(year))
+    if (horas >= d.inicioHoraExtra) {
+      trabajo.horas_extra = horas - d.inicioHoraExtra + 1;
+      trabajo.horas_normal = horas - trabajo.horas_extra;
+    } else {
+      trabajo.horas_normal = horas;
+    }
+    trabajo.fecha = diainicio.add(totaldias, 'days');
+    const fechaddmmm_s = trabajo.fecha.format('DD-MMM').toLowerCase();
+    const diasemana_s = trabajo.fecha.format('ddd').toLowerCase();
+
+    trabajo.diafestivo = (d.diasFestivos.indexOf(fechaddmmm_s) > -1);
+    trabajo.finde = (d.diasFinde.indexOf(diasemana_s) > -1);
+    if (!finde) {
+      let fecha_cambiada = true;
+      while (fecha_cambiada) {
+        const fechaddmmm = trabajo.fecha.format('DD-MMM').toLowerCase();
+        const diasemana = trabajo.fecha.format('ddd').toLowerCase();
+        if ((d.diasFestivos.indexOf(fechaddmmm) > -1) || (d.diasFinde.indexOf(diasemana) > -1)) {
+          trabajo.fecha.add(1, 'days');
+          totaldias++;
+        } else {
+          trabajo.diafestivo = false;
+          trabajo.finde = false;
+          fecha_cambiada = false;
+        }
+      }
+    }
+    trabajoxdia.push(trabajo)
+    totaldias++
+  }
+
+  let plan = [];
+  let precioTotal = 0;
+  for (let td = 0; td < trabajoxdia.length; td++) {
+    const trabajo = trabajoxdia[td];
+    const festivoMulti = trabajo.diafestivo ? (d.precioFestivosMiltiplier || 1) : 1;
+    const findeMulti = trabajo.finde ? (d.precioFindeMiltiplier || 1) : 1;
+    const horas_normales = trabajo.horas_normal
+    const horas_extra = trabajo.horas_extra || 0
+
+    const diaTrabajo = {
+      horas_a_trabajar: dias[td],
+      fecha: trabajo.fecha.locale(d.locale).format('LLLL'),
+      fecha_corta: trabajo.fecha.locale(d.locale).format('DD-MMM-YYYY'),
+      diafestivo: trabajo.diafestivo,
+      findesemana: trabajo.finde,
+
+      horas_a_precio_normal: horas_normales,
+      precio_de_hora_normal: d.precioHoraNormal * findeMulti * festivoMulti,
+      total_por_horas_normal: horas_normales * d.precioHoraNormal * festivoMulti * findeMulti,
+
+      horas_a_precio_extra: horas_extra,
+      precio_de_hora_extra: d.precioHoraExtra * findeMulti * festivoMulti,
+      total_por_horas_extra: horas_extra * d.precioHoraExtra * festivoMulti * findeMulti,
+
+      total_por_dia: 0,
+    }
+
+    diaTrabajo.total_por_dia = diaTrabajo.total_por_horas_normal + diaTrabajo.total_por_horas_extra;
+    precioTotal += diaTrabajo.total_por_dia;
+    plan.push(diaTrabajo);
+  }
+
+  console.log(plan);
+  console.log(precioTotal);
 
   return {
     statusCode: 200,
-    body: JSON.stringify(
-      {
-        message: `Hola, La hora actual es ${ahora}.`,
-        data: datos,
-        input: event,
+    body: JSON.stringify({
+        message: `Plan Generado con éxito`,
+        consulta: q,
+        fecha_de_inicio: `${fecha}-${year}`,
+        total_dias_a_trabajar: dias.length,
+        tiempo_de_entrega_en_dias_calendario: totaldias,
+        horas_a_trabajar_por_dia: horasxdia,
+        total_horas_a_trabajar: totalhoras,
+        precio_total: precioTotal,
+        plan_de_trabajo:plan,
+        parametros: d,
       },
       null,
       2
